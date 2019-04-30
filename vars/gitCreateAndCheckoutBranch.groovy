1e30bf62473e829adce9c224ff266cbef515836f
Jameel Briones <jbriones@redhat.com>
2019-04-30 14:37:10 +0100

    fix: add useExistingBranch param to specify whether to use an existing branch if found or always create branches

--- vars/gitCreateAndCheckoutBranch.groovy
+++ vars/gitCreateAndCheckoutBranch.groovy
@@ -2,17 +2,18 @@
 
 /**
  * @param gitBranch - The git branch name to be created
+ * @param useExistingBranch - Use an existing branch if found
  * @param pushOnCreate - Pushes the branch created locally to the remote git repository if set to true. Defaults to false
  * @return void
  */
-def call(String gitBranch, boolean pushOnCreate = false) {
+def call(String gitBranch, boolean useExistingBranch, boolean pushOnCreate = false) {
   String remoteBranchCommit = sh(returnStdout: true, script: "git ls-remote origin refs/heads/${gitBranch} | cut -f 1").trim()
 
-  if (remoteBranchCommit) {
+  if (remoteBranchCommit && useExistingBranch) {
     sh "git checkout ${gitBranch}"
   } else {
     sh "git checkout -b ${gitBranch}"
-    
+
     if (pushOnCreate) {
       if (params.dryRun) {
         println "Would push the local branch '${gitBranch}' to the remote repository"
@@ -20,5 +21,5 @@ def call(String gitBranch, boolean pushOnCreate = false) {
         sh "git push origin ${gitBranch}"
       }
     }
-  }
+  }  
 }
