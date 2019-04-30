782440e9fc54334000f5ddbfc595362bd5db0220
Jameel Briones <jbriones@redhat.com>
2019-04-30 12:37:37 +0100

    review: make jira utils more generic

--- vars/jiraCreateIssue.groovy
+++ vars/jiraCreateIssue.groovy
@@ -1,18 +1,17 @@
 #!/usr/bin/groovy
 import org.integr8ly.JiraUtils
 
-def call(String jiraCredentials, String productName, String summary, String[] labels, String gitPrUrl) {
+def call(String jiraCredentials, String, url, String query, String body) {
   def jiraUtils = new JiraUtils()
 
-  def (found, key) = jiraUtils.jiraHasIssue(jiraCredentials, labels)
+  def (found, key) = jiraUtils.jiraHasIssue(jiraCredentials, url, query)
   if (!found) {
     if (params.dryRun) {
-      println "Would create a Jira issue for ${productName}: with the summary: ${summary}, PR URL: ${gitPrUrl} and labels: ${labels}"
+      println "Would create a Jira issue for ${productName} with the details: ${body} }"
     } else {
-      println("Creating Jira Issue for ${productName}: ${summary}")
-      jiraUtils.jiraCreateIssue(jiraCredentials, gitPrUrl, labels, summary)
+      key = jiraUtils.jiraCreateIssue(jiraCredentials, url, body)
     }
-  } else {
-    println "Issue already open for ${productName}: https://issues.jboss.org/browse/${key}"
   }
+
+  return key
 }
