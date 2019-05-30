65e790854d9290ed9982500e3a8d31b1bb589d3f
Jameel Briones <jbriones@redhat.com>
2019-05-30 17:22:12 +0100

    change targetRegistryHost name to registryHost

--- vars/getPreReleaseImages.groovy
+++ vars/getPreReleaseImages.groovy
@@ -6,9 +6,9 @@ import org.integr8ly.RegistryImage
  * @param config { 
  *     imageUrls = List of product image urls to check against the target registry
  *     registryCredentials = The secret name to use in request authentication (username + password)
- *     targetRegistryHost = The registry host to check the image against
+ *     registryHost = The registry host to check the image against
  * }
- * @return List of product image urls found in the template directory
+ * @return List of prerelease images
  */
 def call(config) {
   def imageUrls = config.imageUrls ?: []
@@ -16,14 +16,13 @@ def call(config) {
 
   preReleaseImages = imageUrls.collect { imageUrl ->
     def image = new RegistryImage(imageUrl)
-    def params = [
-      credentials: config.registryCredentials,
-      host: config.targetRegistryHost ?: image.getHttpsHost(),
-      image: image.getPath(),
-      tag: image.getTag()
-    ]
-
     try {
+      def params = [
+        credentials: config.registryCredentials,
+        host: config.registryHost ?: image.getHttpsHost(),
+        image: image.getPath(),
+        tag: image.getTag()
+      ]
       registryHasImage(params) ? null : imageUrl
     } catch (Exception e) {
       println "Failed checking ${imageUrl}, adding image as prerelease"
