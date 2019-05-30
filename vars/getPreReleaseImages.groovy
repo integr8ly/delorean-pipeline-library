bfe71b12df80850a61de186964615c9a1abe83af
Jameel Briones <jbriones@redhat.com>
2019-05-30 15:52:46 +0100

    add getProductImages and getPreReleaseImages vars

---
+++ vars/getPreReleaseImages.groovy
@@ -0,0 +1,33 @@
+#!/usr/bin/groovy
+
+/**
+ * @param config { 
+ *     imageUrls = List of product image urls to check against the target registry
+ *     registryCredentials = The secret name to use in request authentication (username + password)
+ *     targetRegistryHost = The registry host to check the image against
+ * }
+ * @return List of product image urls found in the template directory
+ */
+def call(config) {
+  def imageUrls = config.imageUrls ?: []
+  def preReleaseImages = []
+
+  preReleaseImages = imageUrls.collect { imageUrl ->
+    def image = new RegistryImage(imageUrl)
+    def params = [
+      credentials: config.registryCredentials,
+      host: config.targetRegistryHost ?: image.getHttpsHost(),
+      image: image.getPath(),
+      tag: image.getTag()
+    ]
+
+    try {
+      registryHasImage(params) ? null : imageUrl
+    } catch (Exception e) {
+      println "Failed checking ${imageUrl}, adding image to productPreReleaseImages"
+      imageUrl
+    }
+  }.flatten() - null - ''
+  
+  return preReleaseImages
+}
